248876bccd4dd797872c2848e29461d2967ddac6
Chithran <chithranv21cs@psnacet.edu.in>
2024-06-16 13:35:35 +0530

    video

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,7 +4,7 @@ import VideoCard from './VideoCard';
 import ChannelCard from './ChannelCard';
 
 const Videos = ({ videos }) => {
-  console.log(videos[0])
+  console.log("hellovenna",videos)
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
       {videos[0].map((item, idx) => (
